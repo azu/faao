18ebde5941ec32514ba03783fa1c91a9af8ef7fb
azu <azuciao@gmail.com>
2017-06-04 23:21:51 +0900

    feat(electron): support wider webview

--- src/use-case/QuickIssue/OpenGitHubIssueUseCase.ts
+++ src/use-case/QuickIssue/OpenGitHubIssueUseCase.ts
@@ -5,7 +5,7 @@ import { OpenItemInNewTabUseCase } from "../App/OpenItemInNewTabUseCase";
 
 export class OpenGitHubIssueUseCase extends UseCase {
     execute(issueURL: string, title: string = "", body: string = "") {
-        const issueURLFilled = `${issueURL}?title=${title}&body=${body}`;
+        const issueURLFilled = `${issueURL}?title=${encodeURIComponent(title)}&body=${encodeURIComponent(body)}`;
         return this.context.useCase(new OpenItemInNewTabUseCase()).execute(issueURLFilled);
     }
 }
