98f7cc1d2051b996456ddb08fa1277e0b638a9c6
azu <azuciao@gmail.com>
2017-06-11 01:18:36 +0900

    test(domain): add GitHubSearchList tet

---
+++ src/domain/GitHubSearch/GitHubSearchList/__tests__/GitHubSearchList-test.ts
@@ -0,0 +1,22 @@
+// MIT © 2017 azu
+import { GitHubSearchList, GitHubSearchListJSON } from "../GitHubSearchList";
+
+describe("GitHubSearchList", () => {
+    it("json <-> model", () => {
+        const json: GitHubSearchListJSON = {
+            queries: [
+                {
+                    name: "name",
+                    query: "query",
+                    color: "#ffffff",
+                    gitHubSettingId: "1"
+                }
+            ]
+        };
+        const model = GitHubSearchList.fromJSON(json);
+        expect(model).toBeInstanceOf(GitHubSearchList);
+        expect(model.queries).toHaveLength(1);
+        const jsonResult = model.toJSON();
+        expect(jsonResult).toEqual(json);
+    });
+});
