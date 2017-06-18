2deb1635aa44120f1345cef47b80f7c2fa745d08
azu <azuciao@gmail.com>
2017-06-18 15:54:38 +0900

    fix(Stream): support filter in searchList

--- src/use-case/GitHubSearchStream/ApplyFilterToStreamUseCase.ts
+++ src/use-case/GitHubSearchStream/ApplyFilterToStreamUseCase.ts
@@ -6,7 +6,6 @@ import {
 } from "../../infra/repository/GitHubSearchStreamRepository";
 import { AppRepository, appRepository } from "../../infra/repository/AppRepository";
 import { SearchFilterFactory } from "../../domain/GitHubSearch/GitHubSearchStream/SearchFilter/SearchFilterFactory";
-import { GitHubSearchQuery } from "../../domain/GitHubSearch/GitHubSearchList/GitHubSearchQuery";
 
 export const createApplyFilterToStreamUseCase = () => {
     return new ApplyFilterToStreamUseCase(appRepository, gitHubSearchStreamRepository);
@@ -23,16 +22,22 @@ export class ApplyFilterToStreamUseCase extends UseCase {
     execute(filterWord: string) {
         const app = this.appRepository.get();
         const activeQuery = app.user.activity.activeQuery;
+        const activeSearchList = app.user.activity.activeSearchList;
         const activeStream = app.user.activity.activeStream;
-        if (!activeQuery || !activeStream) {
+        if (!activeStream) {
             return;
         }
         const filters = SearchFilterFactory.create(filterWord);
         activeStream.setFilters(filters);
-        if (activeQuery instanceof GitHubSearchQuery) {
+        if (activeSearchList) {
+            return this.gitHubSearchStreamRepository.saveWithSearchList(
+                activeStream,
+                activeSearchList
+            );
+        } else if (activeQuery) {
             return this.gitHubSearchStreamRepository.saveWithQuery(activeStream, activeQuery);
         } else {
-            return this.gitHubSearchStreamRepository.saveWithSearchList(activeStream, activeQuery);
+            return;
         }
     }
 }
