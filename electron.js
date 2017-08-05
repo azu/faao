31fa10b31a2f414d7cae9007210ca8d081c535c5
azu <azuciao@gmail.com>
2017-08-05 15:11:47 +0900

    chore: fix style

--- electron.js
+++ electron.js
@@ -1,14 +1,18 @@
 "use strict";
 const url = require("url");
-const { Menu, app, shell, BrowserWindow } = require('electron');
-const defaultMenu = require('electron-default-menu');
+const { Menu, app, shell, BrowserWindow } = require("electron");
+const defaultMenu = require("electron-default-menu");
+/**
+ * get url for loading BrowserWindow
+ * @returns {string}
+ */
 const getHTMLUrl = () => {
     if (process.env.FAAO_URL) {
         return process.env.FAAO_URL;
     }
     const useRemote = process.argv.indexOf("--use-remote") !== -1;
     if (useRemote) {
-        return "https://azu.github.io/faao/"
+        return "https://azu.github.io/faao/";
     }
     return `file://${__dirname}/public/index.html`;
 };
@@ -17,12 +21,12 @@ if (process.env.NODE_ENV !== "production") {
     console.info(`Open: ${URL}`);
 }
 // context-menu for window
-require('electron-context-menu')();
+require("electron-context-menu")();
 // Standard stuff
-app.on("gpu-process-crashed", (event) => {
+app.on("gpu-process-crashed", event => {
     console.log("gpu-process-crashed", event);
 });
-app.on('ready', () => {
+app.on("ready", () => {
     const menu = defaultMenu(app, shell);
     Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
     // browser-window
@@ -32,7 +36,7 @@ app.on('ready', () => {
         webPreferences: {
             javascript: true,
             plugins: true,
-            nodeIntegration: true,  // We will disable this?
+            nodeIntegration: true, // We will disable this?
             nativeWindowOpen: true,
             webviewTag: true
         }
@@ -41,8 +45,8 @@ app.on('ready', () => {
     mainWindow.loadURL(URL);
 
     // prevent navigation in main webview
-    mainWindow.webContents.once('dom-ready', function(e) {
-        mainWindow.webContents.on('will-navigate', (event, URL) => {
+    mainWindow.webContents.once("dom-ready", function(e) {
+        mainWindow.webContents.on("will-navigate", (event, URL) => {
             const { protocol, hostname } = url.parse(URL);
             if (protocol === "http:" || protocol === "https:") {
                 if (hostname === "localhost") {
@@ -56,8 +60,8 @@ app.on('ready', () => {
     });
 });
 // https://github.com/electron/electron/blob/master/docs/tutorial/security.md#checklist
-app.on('web-contents-created', (event, contents) => {
-    contents.on('will-attach-webview', (event, webPreferences, params) => {
+app.on("web-contents-created", (event, contents) => {
+    contents.on("will-attach-webview", (event, webPreferences, params) => {
         // Strip away preload scripts if unused or verify their location is legitimate
         delete webPreferences.preload;
         delete webPreferences.preloadURL;
