f2af461018b37a7f8208556ce4d9e28555599e81
azu <azuciao@gmail.com>
2017-09-18 17:24:15 +0900

    update almin@0.14

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,11 @@ const dispatcher = new Dispatcher();
 // context connect dispatch with stores
 const context = new Context({
     dispatcher,
-    store: appStoreGroup
+    store: appStoreGroup,
+    options: {
+        strict: true,
+        performanceProfile: true
+    }
 });
 // setup localForage
 localForage.config({
