0cbdfa3658e50669609a4d5e63a06bd85b65140e
azu <azuciao@gmail.com>
2019-05-03 23:18:09 +0900

    fix(electron): use path

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,6 @@
 // MIT © 2019 azu
 "use strict";
 require("ts-node").register({
-    project: "../tsconfig.electron.json"
+    project: require("path").join(__dirname, "../tsconfig.electron.json")
 });
 require("../main-src/electron");
