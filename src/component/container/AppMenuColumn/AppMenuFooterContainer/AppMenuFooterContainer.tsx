328895c21a35c31a1e65464b98d35137d6f308e7
azu <azuciao@gmail.com>
2019-05-03 23:32:11 +0900

    fix(component): fix Footer Setting Button

--- src/component/container/AppMenuColumn/AppMenuFooterContainer/AppMenuFooterContainer.tsx
+++ src/component/container/AppMenuColumn/AppMenuFooterContainer/AppMenuFooterContainer.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { GitHubSearchListState } from "../../../../store/GitHubSearchListStore/GitHubSearchListStore";
 import { BaseContainer } from "../../BaseContainer";
-import { CommandBar } from "office-ui-fabric-react";
+import { CommandBarButton } from "office-ui-fabric-react";
 import { GitHubSettingState } from "../../../../store/GitHubSettingStore/GitHubSettingStore";
 import { AppState } from "../../../../store/AppStore/AppStore";
 import { OpenProfileWindowUseCase } from "../../../../use-case/Profile/ToggleProfileWindowUseCase";
@@ -17,47 +17,52 @@ export interface AppMenuFooterContainerProps {
 }
 
 export class AppMenuFooterContainer extends BaseContainer<AppMenuFooterContainerProps, {}> {
-    menuItems = [
-        {
-            key: "newSearchList",
-            name: "Add New SearchList",
-            iconProps: {
-                iconName: "EditMirrored"
-            },
-            ariaLabel: "Add new SearchList",
-            onClick: () => {
-                this.useCase(new OpenSearchListPanelUseCase()).executor(useCase =>
-                    useCase.execute()
-                );
-            }
-        },
-        {
-            key: "profile",
-            name: "Imports/Exports Profile",
-            iconProps: {
-                iconName: "Settings"
-            },
-            ariaLabel: "Imports/Exports Profile",
-            onClick: () => {
-                (async () => {
-                    await this.useCase(new OpenProfileWindowUseCase()).executor(useCase =>
-                        useCase.execute()
-                    );
-                    await this.useCase(createExportProfileUseCase()).executor(useCase =>
-                        useCase.execute()
-                    );
-                })();
-            }
-        }
-    ];
-
-    farItems = [];
-
     render() {
         return (
-            <header className="AppMenuFooterContainer">
-                <CommandBar items={this.menuItems} farItems={this.farItems} />
-            </header>
+            <footer className="AppMenuFooterContainer">
+                <CommandBarButton
+                    className={"AppMenuFooterContainer-optionButton"}
+                    data-automation-id="AppMenuFooterContainer"
+                    disabled={false}
+                    iconProps={{ iconName: "Settings" }}
+                    text="Options"
+                    menuProps={{
+                        items: [
+                            {
+                                key: "newSearchList",
+                                name: "Add New SearchList",
+                                iconProps: {
+                                    iconName: "EditMirrored"
+                                },
+                                ariaLabel: "Add new SearchList",
+                                onClick: () => {
+                                    this.useCase(new OpenSearchListPanelUseCase()).executor(
+                                        useCase => useCase.execute()
+                                    );
+                                }
+                            },
+                            {
+                                key: "profile",
+                                name: "Imports/Exports Profile",
+                                iconProps: {
+                                    iconName: "Settings"
+                                },
+                                ariaLabel: "Imports/Exports Profile",
+                                onClick: () => {
+                                    (async () => {
+                                        await this.useCase(new OpenProfileWindowUseCase()).executor(
+                                            useCase => useCase.execute()
+                                        );
+                                        await this.useCase(createExportProfileUseCase()).executor(
+                                            useCase => useCase.execute()
+                                        );
+                                    })();
+                                }
+                            }
+                        ]
+                    }}
+                />
+            </footer>
         );
     }
 }
